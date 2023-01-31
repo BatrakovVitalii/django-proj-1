43a69e60f274ce35a5ab948be5c94ee758e451c8
Vitalii Batrakov <vfingerz@yandex.ru>
2023-01-31 20:59:16 +0500

    add Orders, Topping tables in DB and add ManyToMany foreign keys

--- pr123/interfaces/models.py
+++ pr123/interfaces/models.py
@@ -4,8 +4,17 @@ from django.db import models
 
 
 class Coffee(models.Model):
-
     name = models.CharField(max_length=64)
     V = models.IntegerField(blank=False)
     price = models.IntegerField(blank=False)
+    coffee_list = models.ManyToManyField('Topping')
 
+class Topping(models.Model):
+    title_topping = models.CharField(max_length=32)
+    price = models.IntegerField(blank=True, default=0)
+
+class Orders(models.Model):
+    chat_id = models.IntegerField()
+    client_name = models.CharField(max_length=24)
+    total_coasts = models.FloatField(null=True)
+    order_list = models.ManyToManyField('Coffee')
--- pr123/interfaces/views.py
+++ pr123/interfaces/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from interfaces.models import Coffee
+from interfaces.models import Coffee, Orders
 
 menu = ['Главная страница', 'Статистика']
 def order(request):
     coffee = Coffee.objects.all()
+    coffee_order = Orders.objects.all()
     return render(request, "index.html", {'menu': menu,
                                           'title': 'order',
-                                          'client': coffee})
+                                          'client': coffee,
+                                          'coffee_order': coffee_order})
 
 
